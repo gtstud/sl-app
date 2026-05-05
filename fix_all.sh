79ef300f5f4ffc548983816d2997f7bbf75e04cf
sigfridvonshrink <145069841+sigfridvonshrink@users.noreply.github.com>
2026-05-05 10:05:13 +0200

    Delete fix_all.sh

--- fix_all.sh
+++
@@ -1,5 +0,0 @@
-sed -i 's/regex_mismatch/whitelist_mismatch/g' email_handler.py
-sed -i 's/regex_mismatch/whitelist_mismatch/g' tests/test_email_handler.py
-sed -i 's/toggle-contact-regex/toggle-contact-whitelist/g' templates/dashboard/alias_contact_manager.html
-sed -i 's/regex-label-/whitelist-label-/g' templates/dashboard/alias_contact_manager.html
-sed -i 's/Regex mismatch:/Whitelist mismatch:/g' email_handler.py
